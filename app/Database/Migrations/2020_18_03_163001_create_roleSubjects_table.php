ee40187a9d9e02552b1781874e09340e63c0173f
Brad Martin <bradmartin@brads-macbook-2.lan>
2020-03-05 11:30:35 +1100

    Init

--- app/Database/Migrations/2020_18_03_163001_create_roleSubjects_table.php
+++ app/Database/Migrations/2020_18_03_163001_create_roleSubjects_table.php
@@ -16,15 +16,11 @@ class CreateRoleSubjectsTable extends Migration
 
         Schema::create('roleSubjects', function (Blueprint $table) {
 
-          $table->integer('role_id')->unsigned();
-          $table->integer('roleSubjects_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->integer('roleSubjects_id')->unsigned();
             $table->text('roleSubjects_type');
             
-           
-            //FOREIGN KEY CONSTRAINTS
-    
-            //SETTING THE PRIMARY KEYS
-            $table->primary(['roleSubjects_id','role_id','roleSubjects_type']);
+ 
             $table->timestamp('createdAt')->useCurrent();
             $table->timestamp('updatedAt')->nullable();
         });
